44570331d2e9299c4074f92bc05885a36b1a0daf
Christian <christian.knudsen@gmail.com>
2024-03-20 22:07:02 +0100

    Styling mangler

--- TFCD/app.R
+++ TFCD/app.R
@@ -9,11 +9,32 @@
 
 library(shiny)
 library(lubridate)
+source("segment7.R")
 
 # skaber svg'er af 0 og 1
-source("segment7.R")
+
 svg0 <- segment7(0)
-svg1 <- segment1(1)
+svg1 <- segment7(1)
+
+start <- '<style>
+  .svg-container {
+    display: flex;
+  }
+
+svg {
+  width: 100px;
+  height: auto;
+  margin-right: 0px;
+}
+
+.svg-container svg:last-child {
+  margin-right: 0;
+}
+</style>
+  
+  <div class="svg-container">'
+
+slut <- '</div>'
 
 to_binary_string <- function(number) {
   if(number == 0) {
@@ -30,13 +51,18 @@ to_binary_string <- function(number) {
   return(binary_string)
 }
 
+to_svgs <- function(number){
+  temp <- to_binary_string(number)
+  temp <- unlist(strsplit(temp, ""))
+  temp <- paste0(ifelse(temp == 0, svg0, svg1), collapse = " ")
+}
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
     titlePanel("leaving ground"),
-    textOutput("curr_down")
+    htmlOutput("curr_down")
     
 
 )
@@ -45,15 +71,15 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-    output$curr_down <- renderText({
+    output$curr_down <- renderUI({
       invalidateLater(1000, session)
       curr_time <- with_tz(now("UTC"), tzone = "Europe/Copenhagen")
       target_time <-  as.POSIXct("2038-06-30 23:59:59", tz = "Europe/Copenhagen")
       diff <- floor(as.numeric(difftime(target_time, curr_time, units = "secs")))
       if(diff>0){
-        paste(to_binary_string(diff))
+        HTML(paste0(start, to_svgs(diff), slut, collapse = " "))
       }else{
-        paste("Done!")
+        HTML(paste("Done!"))
       }
       
       
@@ -61,6 +87,13 @@ server <- function(input, output, session) {
       
     })
 }
+
+
+
+
+
+
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
