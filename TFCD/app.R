9cb5984d768660c83ff5305caa9d8dc1d8a7efcb
Christian <christian.knudsen@gmail.com>
2024-09-11 19:39:25 +0200

    let oprydning

--- TFCD/app.R
+++ TFCD/app.R
@@ -1,11 +1,4 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+
 
 library(shiny)
 library(lubridate)
@@ -122,7 +115,7 @@ ui <- fluidPage(
 
 
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output, session) {
     output$curr_down <- renderUI({
       invalidateLater(1000, session)
