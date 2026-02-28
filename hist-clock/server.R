a1d9247dab8e50ad23cd0fba659ee3a206778b2d
Christian Knudsen <chrbknudsen@proton.me>
2026-02-28 14:03:34 +0100

    initial commit

---
+++ hist-clock/server.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+
+install.packages("tidyverse")
+
+tribble(~tid, ~begivenhed, ~event, ~link,
+  "0732", "Karl Martel slår maurerne tilbage ved Tours", "Charles Martel repels the Moors at Tours", "dummy",
+  "1492", "De katolske konger erobrer Granada og afslutter den muslimske kolonisering af den Iberiske halvø", "The Catholic Monarchs recapture Granada and ends the muslim colonization of the Iberian peninsula", "dummy",
+  "1812", "Canada brænder Det Hvide hus", "Canada burn down the White House", "dummy")
